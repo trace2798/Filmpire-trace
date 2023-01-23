fbff8a256f1d8ede886f1abbd6dc5811a81c4729
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-01-23 11:07:43 +0100

    Implemented Movie Information and also search while clicking genre in movie description

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,9 +1,93 @@
 import React from 'react'
+import { Modal, Typography, Button, ButtonGroup, CircularProgress, Grid, Box, useMediaQuery, Rating } from '@mui/material'
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favourite, FavouriteBorderOutlined, Remove, ArrowBack } from '@mui/icons-material'
+import { Link, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios';
+import { useGetMovieInformationQuery } from '../../services/TMDB'
+import useStyles from './styles';
+import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
+import genreIcons from '../../assets/genres';
+
 
 const MovieInformation = () => {
-    console.log('Movie Information')
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  console.log('Movie Information')
+  //we will use usePrams to get the movie id
+  const { id } = useParams();
+
+  const { data, error, isFetching } = useGetMovieInformationQuery(
+    id
+  );
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+  console.log(data)
+  if(!data){
+    return (
+      <h4>
+        Id does not match with any on database.
+      </h4>
+      ) 
+  }
+  // if (!data.id) {
+  //   return (
+  //     <Box display="flex" alignItems="center" mt="20px">
+  //       <Typography variant="h4">
+  //         No Movies that match that name
+  //         <br />
+  //         Please search for something else.
+  //       </Typography>
+  //     </Box>
+  //   );
+  // }
+
+  if (error) return "An error occurred";
+  
   return (
-    <div>MovieInformation</div>
+    <Grid container className={classes.containerSpaceAround}>
+      {/* {data.overview} */}
+      <Grid item sm={12} lg={4}>
+       <img
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          alt={data?.title}
+        /> 
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+      <Typography variant="h3" align="center" gutterBottom>
+          {data?.title} ({data.release_date.split('-')[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <Grid item className={classes.containerSpaceAround}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography gutterBottom variant="subtitle1" style={{ marginLeft: '10px' }}>
+              {data?.vote_average} / 10
+            </Typography>
+          </Box>
+          {/* <Typography gutterBottom variant="h6" align="center">{data?.runtime}min {data?.spoken_language.length > 0 ? `/ ${data?.spoken_languages[0].name}` : ''}  </Typography> */}
+          <Typography gutterBottom variant="h6" align="center">{data?.runtime}min</Typography>
+        </Grid>
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre) => (
+            <Link className={classes.links} key={genre.name} to="/" onClick={() => dispatch(selectGenreOrCategory(genre.id))}>
+              <img src={genreIcons[genre.name.toLowerCase()]} className={classes.genreImage} height={30} />
+              <Typography color="textPrimary" variant="subtitle1">{genre?.name}</Typography>
+            </Link>
+          ))}
+        </Grid>
+        
+      </Grid>
+    </Grid>
   )
 }
 
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -18,12 +18,10 @@ export const tmdbApi = createApi({
     getMovies: builder.query({
       query: ({ genreIdOrCategoryName, page, searchQuery }) => {
         console.log("TMDB here");
-
         //Get Movies by search
         if(searchQuery) {
           return `search/movie?api_key=${tmdbApiKey}&query=${searchQuery}&page=${page}`;
         }
-
         //Get movies by category
         if (genreIdOrCategoryName && typeof genreIdOrCategoryName === "string") { 
           return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
@@ -33,15 +31,20 @@ export const tmdbApi = createApi({
           // return `discover/movie?with_genres=${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
           return `discover/movie?api_key=${tmdbApiKey}&page=${page}&with_genres=${genreIdOrCategoryName}`;          
         }
-
         //Get Popular movie
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
-        
       },
     }),
+    //Get movie information
+    getMovieInformation: builder.query({
+      query: (id) => {
+          return `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`;
+      }
+    }),
+
   }),
 });
 
-export const { useGetMoviesQuery, useGetGenresQuery } = tmdbApi;
+export const { useGetMoviesQuery, useGetGenresQuery, useGetMovieInformationQuery } = tmdbApi;
 
-//typeof term is important, withit the if statements were not working. 
+//"typeof" term is important, without it the if statements were not working. 
